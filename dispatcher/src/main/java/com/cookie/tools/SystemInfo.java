24af9c56877477d7c9fba383fef881b1fab59997
Marco <intensework443@gmail.com>
2026-04-24 18:06:12 +0200

    feat: added maven config file and using javafx

---
+++ dispatcher/src/main/java/com/cookie/tools/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.cookie.tools;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ dispatcher/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.cookie.tools {
+    requires javafx.controls;
+    exports com.cookie.tools;
+}
