1bdfa43334cb300cf56b81a15585a2089766ca8c
Marco <intensework443@gmail.com>
2026-04-24 21:21:46 +0200

    feat: update JavaFX version, add internationalization support and scene properties

--- dispatcher/src/main/java/com/cookie/tools/App.java
+++ dispatcher/src/main/java/com/cookie/tools/App.java
@@ -1,9 +1,11 @@
 package com.cookie.tools;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import com.cookie.tools.managers.SceneManager;
+
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
@@ -14,13 +16,25 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
+        Locale locale = Locale.forLanguageTag("it-IT");
+        ResourceBundle bundle = ResourceBundle.getBundle("com.cookie.tools.i18n.language", locale);
+
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
-
+        /* 
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         var scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(scene);
         stage.show();
+        */
+        final double MIN_WIDTH = 750;
+        final double MIN_HEIGHT = 500;
+
+        // LanguageManager.getInstance();               // Inizializza il LanguageManager
+        // Inizializza lo SceneManager
+        SceneManager.getInstance()
+                .loadScene(stage, SceneManager.SceneKeys.MAIN_MENU_VIEW, 
+                    bundle.getString("app.title"), MIN_WIDTH, MIN_HEIGHT);
     }
 
     public static void main(String[] args) {
